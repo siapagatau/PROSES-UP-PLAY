1f7d5dfb7d906c087bc81cb416f375482242b783
siapagatau <254110405116@mhs.uinsaizu.ac.id>
2026-09-14 18:22:29 +0700

    Update version code and name in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dtxpads.kendang"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
